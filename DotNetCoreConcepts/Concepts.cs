034e0e84bdbdb84dab2253dedbf3300fdd64f819
Abbas <abbassha28@gmail.com>
2023-11-19 23:42:11 +0530

    Added the Concepts of Extensions Method and Readonly Vs Const

---
+++ DotNetCoreConcepts/Concepts.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCoreConcepts
+{
+    #region Extension Method
+    // Here Geek class contains three methods 
+    // Now we want to add two more new methods in it  
+    // Without re-compiling this class 
+    class ExtensionMethod
+    {
+
+        // Method 1 
+        public void M1()
+        {
+            Console.WriteLine("Method Name: M1");
+        }
+
+        // Method 2 
+        public void M2()
+        {
+            Console.WriteLine("Method Name: M2");
+        }
+
+        // Method 3 
+        public void M3()
+        {
+            Console.WriteLine("Method Name: M3");
+        }
+
+    }
+
+    // This class contains M4 and M5 method 
+    // Which we want to add in Geek class. 
+    // NewMethodClass is a static class 
+    static class NewMethodClass
+    {
+
+        // Method 4 
+        //Binding parameters are those parameters which are used to bind the new method with the existing class or structure.
+        //It does not take any value when you are calling the extension method because they are used only for binding not for any other use.
+        //always present at the first place, any other place rather than first place then the compiler will give an error.
+        //The binding parameter is created using this keyword followed by the name of the class in which you want to add a new method and the parameter name.
+
+        public static void M4(this ExtensionMethod g)
+        {
+            Console.WriteLine("Method Name: M4");
+        }
+
+        // Method 5 
+        public static void M5(this ExtensionMethod g, string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+    #endregion
+
+    #region ReadOnly vs Const
+    #region Constant
+    //constant fields are created using const keyword.
+    //Const  is a compile time constant.
+    //The value of the const field can not be changed.
+    //It can be declared inside the method.
+    //In const fields, we can only assign values in declaration part.
+    //It cannot be used with static modifiers.
+    class Const
+    {
+        // Constant fields
+        public const int myvar = 10;
+        public const string str = "GeeksforGeeks";
+    }
+    #endregion
+
+    #region ReadOnly
+    //Readonly fields can be created using readonly keyword
+    //ReadOnly is a runtime constant. It cannot be declared inside the method.
+    //The value of readonly field can be changed.
+    //It can be used with static modifiers.
+    class ReadOnly
+    {
+        // readonly variables
+        public readonly int myvar1;
+        public readonly int myvar2;
+
+        // variables are assigned Using constructor
+        public ReadOnly(int b, int c)
+        {
+            myvar1 = b;
+            myvar2 = c;
+            Console.WriteLine("Display value of myvar1 {0}, " +
+                            "and myvar2 {1}", myvar1, myvar2);
+        }
+    }
+    #endregion
+    #endregion
+}
+
--- DotNetCoreConcepts/Program.cs
+++ DotNetCoreConcepts/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotNetCoreConcepts;
+
+#region Extension Method
+//Extension method concept allows you to add new methods in the existing class or in the structure without modifying the source code of the original type
+//It is introduced in C# 3.0.
+ExtensionMethod extension = new ExtensionMethod();
+extension.M1();
+extension.M2();
+extension.M3();
+extension.M4();
+extension.M5("Method Name: M5");
+#endregion
+
+#region Readonly Vs Const 
+// Display the value of Constant fields
+Console.WriteLine("The value of myvar: {0}", Const.myvar);
+Console.WriteLine("The value of str: {0}", Const.str);
+
+ReadOnly readOnly = new ReadOnly(100, 200);
+#endregion
