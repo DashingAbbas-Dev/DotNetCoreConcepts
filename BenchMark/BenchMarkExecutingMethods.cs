7bdc59bc53ef459fb558f8ed40e342f3e1bb404c
Abbas <abbassha28@gmail.com>
2024-02-25 23:09:42 +0530

    BenchMark Console Created

---
+++ BenchMark/BenchMarkExecutingMethods.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Order;
+using BenchmarkDotNet.Reports;
+using System.Data.Common;
+
+namespace BenchMark
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchMarkExecutingMethods
+    {
+        public List<string> RegularList()
+        {
+            return new List<string> { "apple", "banana", "orange" };
+        }
+
+      
+        public List<string> ColExpressionList()
+        {
+            return ["apple", "banana", "orange"];
+        }
+        [Benchmark(Baseline = true)]
+        public bool EndsWithStringValue()
+        {
+            var testString = "serkut";
+            return testString.EndsWith("s");
+        }
+        [Benchmark]
+        public bool EndsWithCharValue()
+        {
+            var testString = "serkut";
+            return testString.EndsWith('s');
+        }
+
+    }
+}
---
+++ BenchMark/Program.cs
@@ -0,0 +1,7 @@
+﻿using BenchMark;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run<BenchMarkExecutingMethods>();
+
+// Display summary
+Console.WriteLine(summary);
